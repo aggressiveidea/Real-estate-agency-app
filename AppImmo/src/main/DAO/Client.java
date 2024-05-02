5b13e8771ddea1214b50ca221c38c0061c7f009a
Basma <biba99riko@gmail.com>
2024-05-02 16:45:49 +0100

    add class client

--- AppImmo/src/main/DAO/Client.java
+++ AppImmo/src/main/DAO/Client.java
@@ -2,4 +2,26 @@ package main.DAO;
 
 public class Client {
     
+    public String client_type;
+
+    //constructor
+    public Client(boolean t) {
+        if (t == true)
+        this.client_type = "Buyer";
+        else this.client_type = "Tenant";
+    }
+
+    public String getclient_type() {
+        return client_type;
+    }
+    public void setclient_type(String client_type) {
+        this.client_type = client_type;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [client_type=" + client_type + "]";
+    }
+
+    
 }
