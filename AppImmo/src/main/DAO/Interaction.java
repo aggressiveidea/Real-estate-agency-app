37aa7fe261da3064ece7a5cb1f36a77895cbbc43
Basma <biba99riko@gmail.com>
2024-05-05 19:48:23 +0100

    method add payment

--- AppImmo/src/main/DAO/Interaction.java
+++ AppImmo/src/main/DAO/Interaction.java
@@ -1,48 +1,91 @@
 package main.DAO;
 
-import java.sql.Time;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.Random;
 
-public class Interaction {
-    
-    public int id, client_id, property_id;
-    public Date date;
-    public Time hour;
-    public String details;
+import javax.swing.JOptionPane;
 
-    //constructor 1
-    public Interaction(int client_id, int property_id, Time hour, String details) {
+enum transaction_type{
+    RENT, BUY;
+}
+
+public class Transaction {
+
+    public int id, property_id, agent_id, client_id, owner_id, contract_id, payment_id;
+    public transaction_type type;
+    public Date date;
+    public double cost;
+
+    //constructor
+    public Transaction(int property_id, int agent_id, int client_id, int owner_id, int contract_id,
+            int payment_id, double cost) {
         
         this.id = generateRandomId();
 
         this.date = new Date();
 
-        this.client_id = client_id;
         this.property_id = property_id;
-        this.hour = hour;
-        this.details = details;
+        this.agent_id = agent_id;
+        this.client_id = client_id;
+        this.owner_id = owner_id;
+        this.contract_id = contract_id;
+        this.payment_id = payment_id;
+        this.cost = cost;
     }
-    
-    //constructor 2
-    public Interaction() {
-        this.id = generateRandomId();
-    }
- 
+    //type in the setter instead of the constructor
+
+    //necessary methods
     private int generateRandomId (){
         Random rand = new Random();
         return rand.nextInt(1000000);
-    }    
+    }  
 
-    //methodes
-    public void AddInteraction ()
-    {
-        //for later
+    private java.sql.Date SqlDate (Date utilDate){
+        return new java.sql.Date (utilDate.getTime());
     }
 
-    public void RemoveInteraction (int id)
-    {
-        //for later
+    //class methods
+    public int generate_contract(int id){
+        return id;
+        //add later
+    }
+
+    public int add_payment (double cost){
+       
+        Payment pay = new Payment(this.id, cost);
+        this.payment_id = pay.id;
+
+        //SQL insert
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "8888");
+
+            String sql = "INSERT INTO Payment (ID, Amount, DatePay) VALUES (?,?,?)";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, this.id);
+            pstmt.setDouble(2, cost);
+            pstmt.setDate(3,SqlDate(this.date));
+
+            // Execute query
+            int rowsInserted = pstmt.executeUpdate();
+
+            if (rowsInserted > 0) {
+                JOptionPane.showMessageDialog(null, "Transaction added successfully", "NOTE",JOptionPane.PLAIN_MESSAGE);
+                return pay.id;
+            } else {
+                JOptionPane.showMessageDialog(null, "Failed to add transaction", "ERROR 1",JOptionPane.ERROR_MESSAGE);
+                return -1;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Failed to add transaction", "ERROR 2",JOptionPane.ERROR_MESSAGE);
+            return -1;
+        }
     }
 
     //getters and setters
@@ -54,6 +97,22 @@ public class Interaction {
         this.id = id;
     }
 
+    public int getProperty_id() {
+        return property_id;
+    }
+
+    public void setProperty_id(int property_id) {
+        this.property_id = property_id;
+    }
+
+    public int getAgent_id() {
+        return agent_id;
+    }
+
+    public void setAgent_id(int agent_id) {
+        this.agent_id = agent_id;
+    }
+
     public int getClient_id() {
         return client_id;
     }
@@ -62,12 +121,46 @@ public class Interaction {
         this.client_id = client_id;
     }
 
-    public int getProperty_id() {
-        return property_id;
+    public int getOwner_id() {
+        return owner_id;
     }
 
-    public void setProperty_id(int property_id) {
-        this.property_id = property_id;
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public int getContract_id() {
+        return contract_id;
+    }
+
+    public void setContract_id(int contract_id) {
+        this.contract_id = contract_id;
+    }
+
+    public int getPayment_id() {
+        return payment_id;
+    }
+
+    public void setPayment_id(int payment_id) {
+        this.payment_id = payment_id;
+    }
+
+    public transaction_type getType() {
+        return type;
+    }
+
+    public void settype (int t){
+        switch (t) {
+            case 1:
+                this.type = transaction_type.RENT;
+                break;
+            case 2:
+                this.type = transaction_type.BUY;
+                break;
+            default:
+                this.type = null;
+                break;
+        }
     }
 
     public Date getDate() {
@@ -78,20 +171,14 @@ public class Interaction {
         this.date = date;
     }
 
-    public Time gethour() {
-        return hour;
+    public double getCost() {
+        return cost;
     }
 
-    public void sethour(Time hour) {
-        this.hour = hour;
+    public void setCost(double cost) {
+        this.cost = cost;
     }
 
-    public String getdetails() {
-        return details;
-    }
-
-    public void setdetails(String details) {
-        this.details = details;
-    }
     
 }
+
