18086d5bd944e74314f1c06e23db9fb69ffcb108
khadidja-Daouadi <khadidja.daouadi06@gmail.com>
2024-05-18 15:01:44 +0100

    some changes in the table
    add column to the table

--- AppImmo/src/main/ui/TableFrame.java
+++ AppImmo/src/main/ui/TableFrame.java
@@ -1,30 +1,25 @@
 package main.ui;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
-public class TableFrame extends JFrame implements ActionListener {
+public class TableFrame extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
     private JTable table;
-    private JButton retour;
 
-    /**
-     * Launch the application.
-     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -38,59 +33,48 @@ public class TableFrame extends JFrame implements ActionListener {
         });
     }
 
-    /**
-     * Create the frame.
-     */
     public TableFrame() {
         setTitle("IMMO");
-        // Ensure the image path is correct
-        setIconImage(Toolkit.getDefaultToolkit().getImage(TableFrame.class.getResource("assets\\logo.png")));
+        setIconImage(Toolkit.getDefaultToolkit().getImage(LandingFrame.class.getResource("assets\\logo.png")));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(963, 630);
         setResizable(false);
 
         contentPane = new JPanel();
+        contentPane.setBackground(new Color(66, 14, 88));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(null);
         setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-
-        JPanel panel = new JPanel();
-        panel.setBounds(0, 0, 949, 603);
-        panel.setBackground(new Color(66, 14, 88));
-        panel.setLayout(null);
-        contentPane.add(panel);
-
-        retour = new JButton("<");
-        retour.setBounds(10, 10, 45, 45);
-        retour.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                LandingFrame mainpage = new LandingFrame();
-                mainpage.setVisible(true);
-                dispose();
-            }
-        });
-        panel.add(retour);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBackground(new Color(115,24,154));
+        scrollPane.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        scrollPane.setBounds(79, 86, 790, 472);
+        contentPane.add(scrollPane);
 
         table = new JTable();
+        table.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        table.setBackground(new Color(255, 255, 255));
+        scrollPane.setViewportView(table);
+        table.setModel(new DefaultTableModel(
+        	new Object[][] {
+        	},
+        	new String[] {
+        		"ID", "Agent ID", "Client ID", "Owner ID", "Property address", "Date"
+        	}
+        ));
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.setCellSelectionEnabled(true);
         table.setColumnSelectionAllowed(true);
-        table.setBounds(79, 90, 790, 468);
-        panel.add(table);
 
-        JLabel lblNewLabel = new JLabel("Liste of appointments");
+        JLabel lblNewLabel = new JLabel("List of appointments ");
         lblNewLabel.setForeground(Color.WHITE);
         lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
         lblNewLabel.setBounds(358, 31, 357, 37);
-        panel.add(lblNewLabel);
+        contentPane.add(lblNewLabel);
 
         JSeparator separator = new JSeparator();
         separator.setBounds(342, 66, 318, 2);
-        panel.add(separator);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        contentPane.add(separator);
     }
 }
