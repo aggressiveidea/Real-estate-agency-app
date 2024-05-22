b37deea146f783cec325f04f61b7e076361fad36
aggressiveidea <imessaoudenealdjia@gmail.com>
2024-05-22 09:55:30 +0200

    changes
    t3 d9i9a al lkhra

--- AppImmo/src/main/DAO/OracleAcc.java
+++ AppImmo/src/main/DAO/OracleAcc.java
@@ -3,5 +3,5 @@ package main.DAO;
 //this is for Oracle account
 public class OracleAcc {
     public static final String USER = "system";
-    public static final String PASS = "SABRINE";
+    public static final String PASS = "aldjia123";
 }
